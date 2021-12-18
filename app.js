452784cf9592f135da823643f9a0dd8c24a57bc1
Dennis <dennis.baumann@hotmail.com>
2021-12-18 13:57:33 +0100

    add butto, css changes

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const userInput = document.getElementById("userInput");
 const ul = document.querySelector('ul');
 
 let itemsArray = localStorage.getItem('items') ?
-JSON.parse(localStorage.getItem('items')) : [];
+    JSON.parse(localStorage.getItem('items')) : [];
 
 itemsArray.forEach(liMaker);
-function liMaker(text){{/**Funktion wenn man auf denn eintrag klickt wird er durchstrichen */}
+
+function liMaker(text) {
+    { /**Funktion wenn man auf denn eintrag klickt wird er durchstrichen */ }
     const li = document.createElement('li')
     li.textContent = "- " + text;
     ul.appendChild(li);
-    li.addEventListener('click', function(){
-            li.style.textDecoration = "line-through";
+    li.addEventListener('click', function() {
+        li.style.textDecoration = "line-through";
     })
 }
 
@@ -24,16 +26,16 @@ closed.addEventListener("click", function() {
     itemsArray = [];
 })
 
-userInput.addEventListener("keydown", function(event){
-    if(event.key == "Enter")
-    add();
+userInput.addEventListener("keydown", function(event) {
+    if (event.key == "Enter") {
+        add();
+    }
 })
 
 
-function add(){
+function add() {
     itemsArray.push(userInput.value);
     localStorage.setItem('items', JSON.stringify(itemsArray));
     liMaker(userInput.value);
     userInput.value = "";
 }
-
